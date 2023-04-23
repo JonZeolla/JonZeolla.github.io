88c535ed629450a87cbae5328ee3d39428d23015
Jon Zeolla <zeolla@gmail.com>
2023-04-23 09:19:23 -0600

    Refactor and another try

--- tests/test_labs.py
+++ tests/test_labs.py
@@ -10,10 +10,13 @@ import subprocess
 import sys
 import time
 from pathlib import Path
+from tkinter import Tk
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 LOG_FORMAT = json.dumps(
     {
@@ -36,6 +39,7 @@ def test_lab_codeblocks() -> None:
 
     for lab in labs:
         code_blocks: list[str] = get_code_from_codeblocks(lab=lab, cwd=cwd)
+        print(code_blocks)
         if os.environ.get("CI") == "true":
             run_code_blocks(code_blocks=code_blocks)
 
@@ -64,38 +68,41 @@ def get_code_from_codeblocks(*, cwd: Path, lab: Path) -> list[str]:
     Get the code in the codeblocks
     """
     lab_file: str = f"file://{cwd}/{lab}"
-    with open(lab_file) as file:
-        options: Options = Options()
-        options.headless = False  # TODO
-        driver = webdriver.Chrome(options=options)
-        driver.get(lab_file)
-
-        # Find the codeblocks that have a copy button
-        copy_buttons = driver.find_elements(
-            By.CSS_SELECTOR, 'button[data-tooltip="Copy"]'
+    options: Options = Options()
+    options.add_argument("--start-maximized")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--headless")
+    prefs: dict[str, int] = {"profile.default_content_setting_values.notifications": 1}
+    options.add_experimental_option("prefs", prefs)
+    driver = webdriver.Chrome(options=options)
+    # Needed to avoid selenium.common.exceptions.ElementNotInteractableException errors
+    wait = WebDriverWait(driver, 15)
+    copy_buttons = wait.until(
+        EC.presence_of_all_elements_located(
+            (By.CSS_SELECTOR, 'button[data-tooltip="Copy"]')
         )
+    )
+    #
+    #     wait = WebDriverWait(driver, 15)
+    #     copy_buttons = wait.until(
+    #         EC.visibility_of_all_elements_located(
+    #             (By.CSS_SELECTOR, 'button[data-tooltip="Copy"]')
+    #         )
+    #     )
+    driver.get(lab_file)
 
-        # Click the copy buttons and add the clipboard contents to the list of codeblocks to return
-        code_blocks: list[str] = []
-        for button in copy_buttons:
-            button.click()
-            time.sleep(0.5)
-            clipboard_content = driver.execute_script(
-                "return navigator.clipboard.readText();"
-            )
-            code_blocks.append(clipboard_content)
-        #             attribute: str = button.get_attribute("data-clipboard-target")
-        #             target = driver.find_element(By.CSS_SELECTOR, attribute)
+    # Find the codeblocks that have a copy button
+    copy_buttons = driver.find_elements(By.CSS_SELECTOR, 'button[data-tooltip="Copy"]')
 
-        # discovered_codeblocks[lab] =
-        # element = driver.find_element_by_xpath('//a[@href="/documentation/webdriver/"]')
-        # element.click() # HTML click method; Does this work?  If not, use the next one which is the js click
-        # driver.execute_script("arguments[0].click();", element)
-        #         code = target  # TODO
+    # Click the copy buttons and add the clipboard contents to the list of codeblocks to return
+    code_blocks: list[str] = []
+    for button in copy_buttons:
+        button.click()
+        time.sleep(0.5)
+        root: Tk = Tk()
+        clipboard_content: str = root.clipboard_get()
+        code_blocks.append(clipboard_content)
 
-        # TODO: to discovered_codeblocks, then run the copy button magic, add that
-        # output to runner list, then iterate through all those commands in order and ensure success
-
-        driver.quit()
+    driver.quit()
 
     return code_blocks
